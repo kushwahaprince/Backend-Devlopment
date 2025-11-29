b2e6cacafb73b0b7cf2f846aad0682bfdd823f5e
kushwahaprince <159607039+kushwahaprince@users.noreply.github.com>
2025-11-29 13:00:40 +0530

    fast day1

---
+++ basic-1/script.js
@@ -0,0 +1,14 @@
+// Fundamental of javascript
+//array and onject
+// functions return
+// async and coding
+// foreach map filter find indexof
+
+/*
+var arr = [1,2,3,4];
+arr.forEach(function(val){
+    console.log(val  + " hello");
+})
+
+*/
+
