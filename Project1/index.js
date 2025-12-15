1ad3c266635e93f800c1610516036b8dac707841
kushwahaprince <159607039+kushwahaprince@users.noreply.github.com>
2025-12-15 23:19:19 +0530

    ....

---
+++ Project1/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path =  require('path');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join(__dirname,'public')));
+// ❗ space hata diya
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    res.render("index"); // index.ejs render hoga
+});
+
+
+app.get("/profile/:username", function (req, res) {
+    res.send("it is working from the profile"); // index.ejs render hoga
+});
+
+app.listen(3000, function () {
+    console.log("its running");
+});
